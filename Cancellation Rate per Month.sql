b355b1e047afa0d39935a5b225c8fcb55efeb49f
Oliver Zhu <ozhu0279@gmail.com>
2026-01-25 18:11:22 +0800

    Create Cancellation Rate per Month.sql

---
+++ Cancellation Rate per Month.sql
@@ -0,0 +1,12 @@
+/*
+Cancellation Rate per Month.
+*/
+SELECT 
+  DATE_FORMAT(invoice_date, '%Y-%m') AS month,
+  COUNT(DISTINCT invoice_no) AS total_orders,
+  COUNT(DISTINCT CASE WHEN quantity < 0 AND invoice_no LIKE 'C%' THEN invoice_no END) AS cancelled_orders,
+  COUNT(DISTINCT CASE WHEN quantity < 0 AND invoice_no LIKE 'C%' THEN invoice_no END)
+    / NULLIF (COUNT(DISTINCT invoice_no),0) AS cancellation_rate
+FROM combined_online_retail
+GROUP BY month
+ORDER BY month;

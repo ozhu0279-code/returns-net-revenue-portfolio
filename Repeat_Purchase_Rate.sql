b22470e6e5f01c12ce1002d399e35643e1c4c802
Oliver Zhu <ozhu0279@gmail.com>
2026-01-27 00:37:14 +0800

    Update Repeat_Purchase_Rate.sql

--- Repeat_Purchase_Rate.sql
+++ Repeat_Purchase_Rate.sql
@@ -24,3 +24,13 @@ WITH orders AS (
        SUM(order_count >= 2) AS repeat_customers,
        SUM(CASE WHEN order_count >= 2 THEN 1 ELSE 0 END) / NULLIF(COUNT(*), 0) AS repeat_rate
    FROM customer_orders;
+
+/*
+Customer coverage rate out of all purchase orders 
+*/
+SELECT
+  COUNT(DISTINCT CASE WHEN quantity > 0 AND unit_price > 0 AND invoice_no NOT LIKE 'C%' THEN invoice_no END) AS total_orders,
+  COUNT(DISTINCT CASE WHEN customer_id IS NOT NULL AND quantity > 0 AND unit_price > 0 AND invoice_no NOT LIKE 'C%' THEN invoice_no END) AS orders_with_customers,
+  COUNT(DISTINCT CASE WHEN customer_id IS NOT NULL AND quantity > 0 AND unit_price > 0 AND invoice_no NOT LIKE 'C%' THEN invoice_no END) 
+  /   NULLIF(COUNT(DISTINCT CASE WHEN quantity > 0 AND unit_price > 0 AND invoice_no NOT LIKE 'C%' THEN invoice_no END), 0) AS customer_id_coverage_rate
+FROM cleaned_final;

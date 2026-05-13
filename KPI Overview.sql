94baa123834fcb8ec2a275872df411cea79adf36
Oliver Zhu <ozhu0279@gmail.com>
2026-05-13 23:48:41 +0800

    Enhance KPI Overview SQL with detailed comments
    Updated comments for clarity and included non-product data in gross/canceled/net revenue calculations.

--- KPI Overview.sql
+++ KPI Overview.sql
@@ -1,4 +1,4 @@
---Gross/Canceled/Net revenue
+--Gross/Canceled/Net revenue (including non-product data)
 WITH base AS (
   SELECT
     invoice_no,
@@ -21,6 +21,50 @@ canceled_r AS (
   FROM base
   WHERE quantity < 0 
     AND invoice_no LIKE 'C%'
+)
+SELECT
+  gr.gross_revenue,
+  cr.canceled_revenue,
+  COALESCE(gr.gross_revenue, 0) - COALESCE(cr.canceled_revenue, 0) AS net_revenue
+FROM gross_r gr, canceled_r cr;
+
+
+----Gross/Canceled/Net revenue (only product data)
+WITH base AS (
+  SELECT
+    invoice_no,
+    quantity,
+    unit_price,
+    stock_code, 
+    quantity * unit_price AS line_amount,
+  CASE 
+    WHEN stock_code IN (
+    'Test001','Test002','S','PADS','Post','M',
+    'Gift_0001_90','Gift_0001_80','Gift_0001_70','Gift_0001_60',
+    'Gift_0001_50','Gift_0001_40','Gift_0001_30','Gift_0001_20',
+    'Gift_0001_10','Gift','DOT','D',
+    'CRUK','C2','C3','BANK CHARGES','B','AMAZONFEE',
+    'ADJUST2','ADJUST'
+    ) THEN 'Non-product'
+    ELSE 'Product'
+  END AS product_type
+ FROM online_retail
+),
+gross_r AS (
+  SELECT
+    SUM(line_amount) AS gross_revenue
+  FROM base
+  WHERE quantity > 0 
+    AND unit_price > 0 
+    AND product_type = 'Product'
+),
+canceled_r AS (
+  SELECT
+    SUM(ABS(line_amount)) AS canceled_revenue
+  FROM base
+  WHERE quantity < 0 
+    AND invoice_no LIKE 'C%'
+    AND product_type = 'Product'
 )
 SELECT
   gr.gross_revenue,

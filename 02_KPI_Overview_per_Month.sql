2ae8b32014fde6a1c4311b9001e88bdb0fb03076
Oliver Zhu <ozhu0279@gmail.com>
2026-05-14 22:53:37 +0800

    Enhance KPI SQL with product-specific revenue calculations
    Added calculations for canceled revenue and net revenue specific to product type.

---
+++ 02_KPI_Overview_per_Month.sql
@@ -0,0 +1,68 @@
+--Gross/Canceled/Net Revenue per month(including non-product data)
+WITH base AS (
+  SELECT
+    invoice_no,
+    DATE_FORMAT(invoice_date, '%Y-%m') AS month,
+    quantity,
+    unit_price,
+    quantity * unit_price AS line_amount
+  FROM online_retail
+)
+SELECT
+  month,
+  SUM(CASE 
+        WHEN quantity > 0 AND unit_price > 0 
+        THEN line_amount ELSE 0 
+      END) AS gross_revenue,
+  
+  SUM(CASE 
+        WHEN quantity < 0 AND invoice_no LIKE 'C%'
+        THEN ABS(line_amount) ELSE 0 
+      END) AS canceled_revenue,
+  
+  SUM(CASE WHEN quantity > 0 AND unit_price > 0 THEN line_amount ELSE 0 END) - 
+  SUM(CASE WHEN quantity < 0 AND invoice_no LIKE 'C%' THEN ABS(line_amount) ELSE 0 END) AS net_revenue
+FROM base
+GROUP BY month
+ORDER BY month;
+
+--Gross/Canceled/Net Revenue per month(only product data)
+WITH base AS (
+  SELECT
+    invoice_no,
+    DATE_FORMAT(invoice_date, '%Y-%m') AS month,
+    quantity,
+    unit_price,
+    quantity * unit_price AS line_amount,
+    CASE 
+      WHEN stock_code IN ('Test001','Test002','S','PADS','Post','M',
+        'Gift_0001_90','Gift_0001_80','Gift_0001_70','Gift_0001_60',
+        'Gift_0001_50','Gift_0001_40','Gift_0001_30','Gift_0001_20',
+        'Gift_0001_10','Gift','DOT','D',
+        'CRUK','C2','C3','BANK CHARGES','B','AMAZONFEE',
+        'ADJUST2','ADJUST'
+      ) THEN 'Non-Product'
+      ELSE 'Product'
+    END AS product_type
+  FROM online_retail
+)
+SELECT
+  month,
+  SUM(CASE 
+        WHEN quantity > 0 AND unit_price > 0 AND product_type = 'Product' 
+        THEN line_amount ELSE 0 
+      END) AS gross_revenue,
+      
+  SUM(CASE 
+        WHEN quantity < 0 AND invoice_no LIKE 'C%' AND product_type = 'Product' 
+        THEN ABS(line_amount) ELSE 0 
+      END) AS canceled_revenue,
+      
+  SUM(CASE WHEN quantity > 0 AND unit_price > 0 AND product_type = 'Product' THEN line_amount ELSE 0 END) - 
+  SUM(CASE WHEN quantity < 0 AND invoice_no LIKE 'C%' AND product_type = 'Product' THEN ABS(line_amount) ELSE 0 END) AS net_revenue
+FROM base
+FROM base
+GROUP BY month
+ORDER BY month;
+
+--Cancel Rate/Canceled Revenue Share per Month(only product data)
